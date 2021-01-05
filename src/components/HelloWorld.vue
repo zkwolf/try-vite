fbb73c494c9f4b0df79eb2b45aaec78e33d2a55d
zkwolf <chenhao5866@gmail.com>
2021-01-05 17:03:25 +0800

    init repo

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <a-layout>
+    <a-button type="primary" style="width: 100%">测试</a-button>
+    <a-spin loading></a-spin>
+    <user-outlined />
+  </a-layout>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from "vue";
+import { UserOutlined } from "@ant-design/icons-vue";
+
+export default defineComponent({
+  name: "HelloWorld",
+  components: {
+    UserOutlined,
+  },
+  props: {
+    msg: {
+      type: String,
+      required: true,
+    },
+  },
+  setup: () => {
+    const count = ref(0);
+    return { count };
+  },
+});
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import Antd from 'ant-design-vue';
+
+import 'ant-design-vue/dist/antd.less';
+
+createApp(App).use(Antd).mount('#app')
